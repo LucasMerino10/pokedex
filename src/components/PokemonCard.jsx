d1d79e9596802f0ca941617e1b5019bb2d3d92b9
LucasMerino10 <lucas.merino9@gmail.com>
2023-10-10 10:40:28 +0200

    only displays pokemon image if available

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,21 @@
 function PokemonCard() {
+    const pokemonList = [
+        {
+            name: "Bulbasaur",
+            imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+        },
+        {
+            name: "Mew",
+        },
+    ];
+    const pokemon = pokemonList[1];
     return (
         <figure className="card">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" 
-            alt="Bulbasaur" 
-            className="card-img"/>
-            <figcaption>Bulbasaur</figcaption>
+            {pokemon.imgSrc ? (
+                <img src={pokemon.imgSrc}
+                className="card-img"/>
+            ) : <p>???</p>}         
+            <figcaption>{pokemon.name}</figcaption>
         </figure>
     )
 }
